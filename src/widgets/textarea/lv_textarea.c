163b118493212fab919735490471a51d9576ba5a
Nicola Orlandi <nicola@1company.one>
2025-09-22 08:54:15 +0200

    Draw the cursor to the next line only if the label does not fit the content
    This way, the cursor does not move to the next line when the textbox is multi-line and the label fits the content.
    We set the width of the label so that it fits the content (width set to LV_SIZE_CONTENT) when we want to disable the text wrap.

--- src/widgets/textarea/lv_textarea.c
+++ src/widgets/textarea/lv_textarea.c
@@ -1194,11 +1194,12 @@ static void refr_cursor_area(lv_obj_t * obj)
     lv_point_t letter_pos;
     lv_label_get_letter_pos(ta->label, cur_pos, &letter_pos);
 
+    int32_t label_width = lv_obj_get_style_width(ta->label, LV_PART_MAIN);
     lv_text_align_t align = lv_obj_calculate_style_text_align(ta->label, LV_PART_MAIN, lv_label_get_text(ta->label));
 
     /*If the cursor is out of the text (most right) draw it to the next line*/
     if(((letter_pos.x + ta->label->coords.x1) + letter_w > ta->label->coords.x2) &&
-       (ta->one_line == 0 && align != LV_TEXT_ALIGN_RIGHT)) {
+       (label_width != LV_SIZE_CONTENT && align != LV_TEXT_ALIGN_RIGHT)) {
 
         letter_pos.x = 0;
         letter_pos.y += letter_h + line_space;
