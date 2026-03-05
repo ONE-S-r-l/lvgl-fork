ca1eb9151ad62c0b795ad2ec875b70c374176ef2
Nicola Orlandi <nicola@oktima.ai>
2026-03-05 15:48:06 +0100

    wip

--- src/draw/nxp/g2d/lv_g2d_buf_map.c
+++ src/draw/nxp/g2d/lv_g2d_buf_map.c
@@ -95,6 +95,7 @@ void g2d_insert_buf_map(void * key, struct g2d_buf * value)
     if(table->items[index] == NULL) {
         /* Key not found. Insert item. */
         table->items[index] = item;
+        LV_LOG_WARN("Add item %p, count %d", item, table->count);
         table->count++;
         return;
     }
@@ -152,6 +153,7 @@ void g2d_free_item(void * key)
         /* Remove the item. */
         table->items[index] = NULL;
         _map_free_item(item);
+        LV_LOG_WARN("Remove item %p, count %d", item, table->count);
         table->count--;
 
         /* If there is no collision chain, just return. */
@@ -177,6 +179,7 @@ void g2d_free_item(void * key)
             if(item->key == key) {
                 g2d_free(item->value);
                 lv_array_remove(list, i);
+                LV_LOG_WARN("Remove item %p, count %d", item, table->count);
                 table->count--;
                 if(lv_array_size(list) == 0) {
                     _map_free_list(index, list);
@@ -232,6 +235,7 @@ static void _handle_collision(unsigned long index, lv_map_item_t * item)
         lv_array_init(list, LV_ARRAY_DEFAULT_CAPACITY, sizeof(lv_map_item_t));
         lv_array_push_back(list, item);
         table->overflow_list[index] = list;
+        LV_LOG_WARN("Add item %p, count %d", item, table->count);
         table->count++;
         return;
     }
@@ -247,6 +251,7 @@ static void _handle_collision(unsigned long index, lv_map_item_t * item)
         }
         /* Insert to the list. */
         lv_array_push_back(table->overflow_list[index], item);
+        LV_LOG_WARN("Add item %p, count %d", item, table->count);
         table->count++;
         return;
     }
